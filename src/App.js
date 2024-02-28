52804b4deb8d38236934f109d51b90e82a5dd527
smorgannicole <morgann.smithh@yahoo.com>
2024-02-28 09:34:21 -0800

    board rendered on screenn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
+import Square from './components/Square'
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -17,6 +18,13 @@ const App = () => {
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <div className="board-wrapper">
+        <div className="board">
+          {board.map((square, index) => {
+            return <Square />
+          })}
+        </div>
+      </div>
     </>
   )
 }
